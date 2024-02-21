56c760e6953f072df83b1c18b004fc039e07a227
Conor <buchkowc@mail.gvsu.edu>
2024-02-21 16:24:38 -0500

    Create the FRUIT class that allows for creating a fruit in a random cell on the gameboard

--- Snake.py
+++ Snake.py
@@ -1,15 +1,29 @@
 import pygame, random
 
+from pygame.math import Vector2
+
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size),cell_size,cell_size)
+        pygame.draw.rect(screen,(206,0,0),fruit_rect)
+
+
 pygame.init()
 
 cell_size = 30
 cell_number = 20
 
-
-
 screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
 
 clock = pygame.time.Clock()
+
+fruit = FRUIT()
 
 player = pygame.Rect((300,250,25,25))
 
@@ -19,6 +33,8 @@ run = True
 while run:
 
     screen.fill((0,0,0))
+
+    fruit.draw_fruit()
 
     pygame.draw.rect(screen, (255,0,0), player)
 
